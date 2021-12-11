a3959aa77cb9fe91f3b18de1b786b246e7755e45
Maxime Dousselaere <maxime.dousselaere@gmail.com>
2021-12-11 22:15:36 +0100

    [hello-world] V1
    its cool

--- hello-world/index.js
+++ hello-world/index.js
@@ -3,19 +3,49 @@
 */
 
     /*
+        Global vals
+    */
+        var fonts = [
+            "Georgia, serif",
+            '"Gill Sans", sans-serif',
+            "sans-serif",
+            "serif",
+            "cursive",
+            "system-ui",
+            "impact",
+            "Times new Roman"
+        ];
+
+
+    /*
         Triggered when a dude click somewhere on the page
     */  
     function onClickBtn(){
         var cX = event.clientX;
         var cY = event.clientY;
-
+    
         var element = document.createElement("span");   
         element.innerHTML = "Hello World !";                  
-
-        element.setAttribute("style", "position:absolute;margin:0px;top:"+cY+"px;left:"+cX+"px;"); // placement sur la page
-
-        document.body.appendChild(element);  // ajout de l'élément
-
         
-        
+       
+
+        element.setAttribute("style", "position:absolute;text-decoration:none;margin:0px;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-o-user-select:none;user-select:none;top:"
+                                +cY+"px;left:"+cX+"px;font-family:"+fonts[getRandom(fonts.length)]+
+                                ";font-size:"+getRandom(50)+"px;color:"+getRandomColor()+""); // style of the element
+        document.body.appendChild(element);  // add element on page
+    }
+
+    /*
+        Return random value from 1 to max
+    */
+    function getRandom( max ){
+        return Math.floor(Math.random() * max);
+    }
+
+    /*
+        Return random hexadecimal code
+    */
+    function getRandomColor(){
+        let n = (Math.random() * 0xfffff * 1000000).toString(16);
+        return '#' + n.slice(0, 6);
     }
