fe7a230376d8b5b7fe6527769c4e201907696652
Maxime Dousselaere <maxime.dousselaere@gmail.com>
2021-12-11 21:38:53 +0100

    [hello-world] {changes} add element creation
    elements can be created by left-click

--- hello-world/index.js
+++ hello-world/index.js
@@ -2,3 +2,20 @@
     Javascript Hello World 
 */
 
+    /*
+        Triggered when a dude click somewhere on the page
+    */  
+    function onClickBtn(){
+        var cX = event.clientX;
+        var cY = event.clientY;
+
+        var element = document.createElement("span");   
+        element.innerHTML = "Hello World !";                  
+
+        element.setAttribute("style", "position:absolute;margin:0px;top:"+cY+"px;left:"+cX+"px;"); // placement sur la page
+
+        document.body.appendChild(element);  // ajout de l'élément
+
+        
+        
+    }
