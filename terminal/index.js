2aa983def5d46354d002e630e4bc2e21d418f68c
Maxime Dousselaere <maxime.dousselaere@gmail.com>
2021-12-13 19:45:05 +0100

    [add] write in cmd
    you can now write in the cmd console

--- terminal/index.js
+++ terminal/index.js
@@ -1,3 +1,30 @@
 /*
     Javascript Terminal
 */
+
+    // global vars 
+    var input = document.getElementById("input");
+    var output = document.getElementById("output");
+
+    // triggers the focus on the input 
+    input.focus();
+
+    /*
+        Initialise le CMD 
+    */
+    function initCMD(){
+        output.value="User : >";
+    }
+
+    initCMD();
+
+    /*
+        Write in cmd
+    */
+   function writeCMD(){
+        var entree = input.value;
+        output.value += " "+entree;
+        output.value += "\nUser : >";
+        input.value="";
+   }
+    
